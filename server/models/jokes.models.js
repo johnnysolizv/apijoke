c1b7ce136c84dd384a558670c422d07613e4ddfb
johnnysolizv <50997730+johnnysolizv@users.noreply.github.com>
2021-11-16 11:58:35 -0300

    act.

--- server/models/jokes.models.js
+++ server/models/jokes.models.js
@@ -4,11 +4,12 @@ const JokeSchema = new mongoose.Schema({
     setup: {
         type: String,
         required: [true, 'El setup es requerido'],
-        minlength: [3, 'El setup debe tener al menos 3 caracteres']
+        minlength: [10, 'El setup debe tener al menos 10 caracteres']
     },
     punchline: {
         type: String,
-        required: [true, 'El punchline es requerido']
+        required: [true, 'El punchline es requerido'],
+        minlength: [3, 'El punchline debe tener al menos 3 caracteres']
     },
 }, { timestamps: true });
 
